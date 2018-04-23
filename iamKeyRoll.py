6dc4f1fd47feee41122e0587201d0423da6d1187
Dana Taylor <danataylor.ct@gmail.com>
2018-04-23 13:41:27 -0400

    Removed list for existing keys, made single var

--- iamKeyRoll.py
+++ iamKeyRoll.py
@@ -2,30 +2,28 @@ import boto3
 
 user = "" # update accordingly
 profile = "" # ^
-existing_keys = [] # empty global initialization
 
-client = boto3.Session(profile_name=profile).client('iam')
+client = boto3.Session(profile_name=profile).client('iam') # connection initialization
 
-def list_previous (client, user, existing_keys): # list existing keys, add them to a list
+def list_previous (client, user): # access existing key, add to global variable
+	global existing_key
 	response = client.list_access_keys(UserName=user)
 	for item in response["AccessKeyMetadata"]:
-		existing_keys.append(item["AccessKeyId"])
+		existing_key = item["AccessKeyId"]
 
-def create_new (client, user): # create new keys
+def create_new (client, user): # create new key
 	creation = client.create_access_key(UserName=user)
 
-def disable_previous (client, user, existing_keys): # set existing keys as inactive
-	for key in existing_keys:
-		client.update_access_key(AccessKeyId=key, Status="Inactive", UserName=user)
+def disable_previous (client, user, existing_key): # set old key as inactive
+	client.update_access_key(AccessKeyId=existing_key, Status="Inactive", UserName=user)
 
-def delete_previous (client, user, existing_keys): # delete the old, inactive keys
-	 for key in existing_keys:
-	 	client.delete_access_key(UserName=user, AccessKeyId=key)
+def delete_previous (client, user, existing_key): # delete the old, inactive key
+	client.delete_access_key(UserName=user, AccessKeyId=existing_key)
 
 def main():
-	list_previous(client, user, existing_keys)
+	list_previous(client, user)
 	create_new(client, user)
-	disable_previous(client, user, existing_keys)
-	delete_previous(client, user, existing_keys)
+	disable_previous(client, user, existing_key)
+	delete_previous(client, user, existing_key)
 
 main()
